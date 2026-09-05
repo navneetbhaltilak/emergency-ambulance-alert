64e1c6e71b6d64170b4c7ebed05cc3663fd0589d
navneetbhaltilak <bhaltilaknavneet@gmail.com>
2026-09-05 12:29:36 +0530

    added comment(//) to a uncommented line

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,4 @@
-
-Firebase messaging sw · JS
+// Firebase messaging service worker
 importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js");
  
